678632b092deea560dd4e041a5cab97b1944289e
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-10-05 16:06:26 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -32,23 +32,86 @@ def compare_audio(teacher_path, student_path, distance_function):
     return score
 
 
-def main():
-    st.write("""# String Sync""")
-    teacher_path = "teacher.m4a"
-    student_paths = [f for f in os.listdir() if f.endswith('.m4a')]
+def audio_display(filename):
+    # Generate a unique ID for the audio element
+    audio_id = st.empty().audio(filename, format='audio/wav')
 
+    # Function to handle the button click event
+    def play_audio():
+        with open(filename, 'rb') as audio_file:
+            audio_bytes = audio_file.read()
+        audio_id.audio(audio_bytes, format='audio/wav')
+
+
+def main():
+    # Add custom CSS for table borders and left alignment
+    st.markdown(
+        """
+        <style>
+            .stTable {
+                text-align: left !important;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    st.write("""# String Sync""")
+
+    # Assuming teacher_files is a list of all teacher audio files
+    teacher_files = [f for f in os.listdir() if f.startswith('teacher') and f.endswith('.m4a')]
+
+    # Create a header
+    col1, col2, col3 = st.columns([0.4, 0.5, 0.1])
+    with col1:
+        st.write("Lesson")
+    with col2:
+        st.write("Student Recording")
+    with col3:
+        st.write("Score")
+
+    for teacher_file in teacher_files:
+        # Create columns for Teacher File, Upload Button, and Distance
+        col1, col2, col3 = st.columns([0.4, 0.5, 0.1])
+
+        # Display teacher file in the first column
+        with col1:
+            st.write("")
+            st.write("")
+            st.audio(teacher_file, format='audio/m4a')
+
+        # Display file uploader in the second column
+        with col2:
+            uploaded_student_file = st.file_uploader(f"", type=["m4a", "wav", "mp3"])
+
+        # Process the uploaded student file and display the distance in the third column
+        with col3:
+            if uploaded_student_file is not None:
+                student_path = "student.m4a"
+
+                with open(student_path, "wb") as f:
+                    f.write(uploaded_student_file.getbuffer())
+
+                distance = compare_audio(teacher_file, student_path, cosine)
+                st.write("")
+                st.write(f"{distance}")
+
+                # Optionally, you can delete the uploaded student file after processing
+                os.remove(student_path)
+
+
+def process_student_files(teacher_path):
+    student_paths = [f for f in os.listdir() if f.endswith('.m4a')]
     # Initialize an empty DataFrame
     df = pd.DataFrame(columns=["Student File", "Final Score"])
-
     print("+----------------+--------------+")
     print("| Student File   | Final Score  |")
     print("+----------------+--------------+")
-
+    audio_display("teacher.m4a")
     for student_path in student_paths:
         score = compare_audio(teacher_path, student_path, cosine)
         new_row = pd.DataFrame({"Student File": [student_path], "Final Score": [score]})
         df = pd.concat([df, new_row], ignore_index=True)
-
     # Display the DataFrame in the console
     st.write(df)
 
