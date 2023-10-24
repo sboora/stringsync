22d6b3fe7571febca914ca5b6c388c371837ecda
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-10-24 12:06:25 -0700

    Added badges enum

---
+++ enums/Badges.py
@@ -0,0 +1,14 @@
+from enum import Enum, auto
+
+
+class Badges(Enum):
+    FIRST_NOTE = "First Note"
+    RISING_STAR = "Rising Star"
+    PRACTICE_MAKES_PERFECT = "Practice Makes Perfect"
+    FAST_LEARNER = "Fast Learner"
+    SONG_BIRD = "Song Bird"
+    MAESTRO = "Maestro"
+    PERFECT_PITCH = "Perfect Pitch"
+    MUSIC_WIZARD = "Music Wizard"
+    ROCKSTAR = "Rockstar"
+    VIRTUOSO = "Virtuoso"
