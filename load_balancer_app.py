8e697f6a665aa220175d14f7c16b4693a0438906
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-10-26 21:50:46 -0700

    Add files via upload

--- load_balancer_app.py
+++ load_balancer_app.py
@@ -1,32 +1,25 @@
 import os
 
 import streamlit as st
-import webbrowser
+import streamlit as st
+from streamlit.components.v1 import html
 
 from repositories.AppInstanceRepository import AppInstanceRepository
 
 
 def main():
-    st.write("Redirecting to cnn.com")
-    webbrowser.open_new_tab("http://www.cnn.com")
-    st.write("Redirected to cnn.com")
-
-def sub():
-    if 'redirect_done' not in st.session_state:
-        st.session_state['redirect_done'] = False  # Initialize redirect flag
-
-    if 'app_instance_url' not in st.session_state and not st.session_state['redirect_done']:
-        set_env()
-        app_instance_repo = AppInstanceRepository()
-        st.write("Load balancer active")
-        app_instance = app_instance_repo.get_earliest_instance()
-        app_instance_url = app_instance['url']
-        app_instance_repo.update_last_used(app_instance['id'])
-        st.session_state['app_instance_url'] = app_instance_url  # Store in session state
-        st.write(app_instance_url)
-        app_instance_url = "www.google.com"
-        webbrowser.open_new_tab(app_instance_url)
-        st.session_state['redirect_done'] = True  # Set redirect flag to true
+    set_env()
+    app_instance_repo = AppInstanceRepository()
+    st.write("Load balancer active")
+    app_instance = app_instance_repo.get_earliest_instance()
+    app_instance_url = app_instance['url']
+    app_instance_repo.update_last_used(app_instance['id'])
+    st.write(app_instance_url)
+    link_html = " <a target=\"_self\" href=\"{url}\" >{msg}</a> ".format(
+        url=app_instance_url,
+        msg="Click to continue!"
+    )
+    st.markdown(link_html, unsafe_allow_html=True)
 
 
 def set_env():
