f9fa0236cf9a4d4db7f225dd09e80a9477f1e736
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-10-26 22:38:25 -0700

    Add files via upload

--- portals/BasePortal.py
+++ portals/BasePortal.py
@@ -99,7 +99,7 @@ class BasePortal(ABC):
                         </div>
                     """, unsafe_allow_html=True)
 
-        st.markdown("""<div style="text-align: center; font-size: 16px; "> Embark on a musical journey from novice to 
+        st.markdown("""<div style="text-align: center; font-size: 17px; "> Embark on a musical journey from novice to 
         maestro at GuruShishya, where the age-old guru-shishya tradition fosters endless artistic 
         exploration.</div>""", unsafe_allow_html=True)
 
