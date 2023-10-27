85ac1ace9d6aa0ffc6ca5c537934aac1cbf6df17
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-10-26 21:17:33 -0700

    Add files via upload

--- load_balancer_app.py
+++ load_balancer_app.py
@@ -6,7 +6,10 @@ from repositories.AppInstanceRepository import AppInstanceRepository
 
 
 def main():
-    if 'app_instance_url' not in st.session_state:
+    if 'redirect_done' not in st.session_state:
+        st.session_state['redirect_done'] = False  # Initialize redirect flag
+
+    if 'app_instance_url' not in st.session_state and not st.session_state['redirect_done']:
         set_env()
         app_instance_repo = AppInstanceRepository()
         st.write("Load balancer active")
@@ -17,7 +20,7 @@ def main():
         st.write(app_instance_url)
         app_instance_url = "www.google.com"
         st.write(f'<meta http-equiv="refresh" content="0; URL={app_instance_url}" />', unsafe_allow_html=True)
-
+        st.session_state['redirect_done'] = True  # Set redirect flag to true
 
 def set_env():
     env_vars = ['ROOT_USER', 'ROOT_PASSWORD', 'ADMIN_PASSWORD',
