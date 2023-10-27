ba6bda18c57ca7985be28e2244af68b9e37a4c47
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-10-26 21:07:11 -0700

    Add files via upload

--- load_balancer_app.py
+++ load_balancer_app.py
@@ -6,23 +6,25 @@ from repositories.AppInstanceRepository import AppInstanceRepository
 
 
 def main():
-    set_env()
-    app_instance_repo = AppInstanceRepository()
-    st.write("Load balancer active")
-    app_instance = app_instance_repo.get_earliest_instance()
-    app_instance_url = app_instance['url']
-    app_instance_repo.update_last_used(app_instance['id'])
-    st.write(app_instance_url)
-    app_instance_url = "www.google.com"
-    iframe_code = f"""
-        <iframe
-            src="{app_instance_url}?embed=true"
-            height="450"
-            style="width:100%;border:none;"
-        ></iframe>
-    """
-    st.markdown(iframe_code, unsafe_allow_html=True)
-    return
+    if 'app_instance_url' not in st.session_state:
+        set_env()
+        app_instance_repo = AppInstanceRepository()
+        st.write("Load balancer active")
+        app_instance = app_instance_repo.get_earliest_instance()
+        app_instance_url = app_instance['url']
+        app_instance_repo.update_last_used(app_instance['id'])
+        st.session_state['app_instance_url'] = app_instance_url  # Store in session state
+        st.write(app_instance_url)
+        app_instance_url = "www.google.com"
+        iframe_code = f"""
+                <iframe
+                    src="{app_instance_url}?embed=true"
+                    height="450"
+                    style="width:100%;border:none;"
+                ></iframe>
+            """
+        st.markdown(iframe_code, unsafe_allow_html=True)
+
 
 def set_env():
     env_vars = ['ROOT_USER', 'ROOT_PASSWORD', 'ADMIN_PASSWORD',
