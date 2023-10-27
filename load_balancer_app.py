8b7b58e735a9a22fe8fc52b96fa25b1194f528aa
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-10-26 19:31:28 -0700

    Add the load balancer

---
+++ load_balancer_app.py
@@ -0,0 +1,28 @@
+import os
+
+import streamlit as st
+
+from repositories.AppInstanceRepository import AppInstanceRepository
+
+
+def main():
+    set_env()
+    app_instance_repo = AppInstanceRepository()
+    app_instance = app_instance_repo.get_earliest_instance()
+    app_instance_url = app_instance['url']
+    app_instance_repo.update_last_used(app_instance['id'])
+    print(app_instance_url)
+    st.write(f'<meta http-equiv="refresh" content="0; URL={app_instance_url}" />', unsafe_allow_html=True)
+
+
+def set_env():
+    env_vars = ['ROOT_USER', 'ROOT_PASSWORD', 'ADMIN_PASSWORD',
+                'SQL_SERVER', 'SQL_DATABASE', 'SQL_USERNAME', 'SQL_PASSWORD',
+                'MYSQL_CONNECTION_STRING', 'EMAIL_ID', 'EMAIL_PASSWORD']
+    for var in env_vars:
+        os.environ[var] = st.secrets[var]
+    os.environ["GOOGLE_APP_CRED"] = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
+
+
+if __name__ == "__main__":
+    main()
