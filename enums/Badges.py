75cec2b3b5ace44537d14bf02b33ef2809b2d3eb
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-11-01 09:47:52 -0700

    Add files via upload

--- enums/Badges.py
+++ enums/Badges.py
@@ -1,14 +1,22 @@
 from enum import Enum, auto
 
 
-class Badges(Enum):
+class UserBadges(Enum):
     FIRST_NOTE = "First Note"
     RISING_STAR = "Rising Star"
-    PRACTICE_MAKES_PERFECT = "Practice Makes Perfect"
+    ROCKSTAR = "Rockstar"
+    TWO_DAY_STREAK = "2 day streak"
+    THREE_DAY_STREAK = "3 day streak"
+    FIVE_DAY_STREAK = "5 day streak"
+    SEVEN_DAY_STREAK = "7 day streak"
+    TEN_DAY_STREAK = "10 day streak"
+
+
+class TrackBadges(Enum):
     FAST_LEARNER = "Fast Learner"
     SONG_BIRD = "Song Bird"
     MAESTRO = "Maestro"
     PERFECT_PITCH = "Perfect Pitch"
     MUSIC_WIZARD = "Music Wizard"
-    ROCKSTAR = "Rockstar"
     VIRTUOSO = "Virtuoso"
+    PRACTICE_MAKES_PERFECT = "Practice Makes Perfect"
---
+++ enums/Settings.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+
+class SettingType(Enum):
+    INTEGER = "Integer",
+    FLOAT = "Float",
+    BOOL = "Bool",
+    TEXT = "Text",
+    COLOR = "Color",
+    DATE = "Date"
+
+
+class Portal(Enum):
+    TENANT = "tenant"
+    ADMIN = "admin"
+    TEACHER = "teacher"
+    STUDENT = "student"
+
+
+class Settings(Enum):
+    MIN_SCORE_FOR_EARNING_BADGES = ("Minimum Score For Earning Badges", Portal.TEACHER, SettingType.INTEGER)
+    TAB_BACKGROUND_COLOR = ("Tab Background Color", Portal.TEACHER, SettingType.COLOR)
+    MAX_ROW_COUNT_IN_LIST = ("Max Row Count In List", Portal.TEACHER, SettingType.INTEGER)
+
+    @classmethod
+    def get_by_description(cls, description):
+        return next((item for item in cls if item.description == description), None)
+
+    @property
+    def description(self):
+        return self.value[0]
+
+    @property
+    def portal(self):
+        return self.value[1]
+
+    @property
+    def data_type(self):
+        return self.value[2]
