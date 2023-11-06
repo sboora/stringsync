a48317920add9f6ccfe08a7407c0ceb4cff07d93
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-11-05 16:04:57 -0800

    Add files via upload

--- enums/ActivityType.py
+++ enums/ActivityType.py
@@ -6,3 +6,5 @@ class ActivityType(enum.Enum):
     LOG_OUT = "Log Out"
     PLAY_TRACK = "Play Track"
     UPLOAD_RECORDING = "Upload Recording"
+    REGISTER_TUTOR = "Register Tutor"
+    REGISTER_SCHOOL = "Register School"
