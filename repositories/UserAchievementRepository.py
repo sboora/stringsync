ad76a8e4a087053316488821e397f9279b51993b
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-11-11 17:17:47 -0800

    Badge awarder timestamp fix

--- repositories/UserAchievementRepository.py
+++ repositories/UserAchievementRepository.py
@@ -51,9 +51,9 @@ class UserAchievementRepository:
         else:
             return False, f"User with ID {user_id} already has the {badge.name} badge for the previous week"
 
-    def award_user_badge(self, user_id, badge: UserBadges):
+    def award_user_badge(self, user_id, badge: UserBadges, timestamp=datetime.datetime.now()):
         cursor = self.connection.cursor()
-
+        print(timestamp)
         # Check for existing 'FIRST_NOTE' badge
         if badge == UserBadges.FIRST_NOTE:
             cursor.execute(
@@ -65,8 +65,8 @@ class UserAchievementRepository:
             # Check for other badges awarded today
             cursor.execute(
                 "SELECT COUNT(*) FROM user_achievements "
-                "WHERE user_id = %s AND badge = %s AND DATE(timestamp) = CURDATE()",
-                (user_id, badge.value)
+                "WHERE user_id = %s AND badge = %s AND DATE(timestamp) = DATE(%s)",
+                (user_id, badge.value, timestamp)
             )
 
         existing_badges = cursor.fetchone()
@@ -75,15 +75,16 @@ class UserAchievementRepository:
         if existing_badges[0] == 0:
             # Award the new badge
             cursor.execute(
-                "INSERT INTO user_achievements (user_id, badge) VALUES (%s, %s)",
-                (user_id, badge.value)
+                "INSERT INTO user_achievements (user_id, badge, timestamp) VALUES (%s, %s, %s)",
+                (user_id, badge.value, timestamp)
             )
             self.connection.commit()
             return True, f"Awarded {badge.name} to user with ID {user_id}"
         else:
             return False, f"User with ID {user_id} already has the {badge.name} badge"
 
-    def award_track_badge(self, user_id, recording_id, badge: TrackBadges):
+    def award_track_badge(self, user_id, recording_id, badge: TrackBadges,
+                          timestamp=datetime.datetime.now()):
         cursor = self.connection.cursor()
         # Enforce badge uniqueness at the track level
         cursor.execute(
@@ -96,8 +97,9 @@ class UserAchievementRepository:
         if existing_badges[0] == 0:
             # Award the new badge
             cursor.execute(
-                "INSERT INTO user_achievements (user_id, badge, recording_id) VALUES (%s, %s, %s)",
-                (user_id, badge.value, recording_id)
+                "INSERT INTO user_achievements (user_id, badge, recording_id, timestamp) "
+                "VALUES (%s, %s, %s, %s)",
+                (user_id, badge.value, recording_id, timestamp)
             )
             self.connection.commit()
             return True, f"Awarded {badge.value} to user with ID {user_id}"
@@ -124,6 +126,3 @@ class UserAchievementRepository:
             return badge[0]
 
         return None
-
-
-
