87a119598a0f2f7ceffc34d2b3ba6ce5c4a0617f
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-11-21 11:25:55 -0800

    Cosmetic changes

--- core/ListBuilder.py
+++ core/ListBuilder.py
@@ -22,9 +22,11 @@ class ListBuilder:
         row_html = "<div style='padding:5px;border-radius:3px;border:1px solid black;'>"
         row_html += col0
         for (column_name, value), width in zip(row_data.items(), self.column_widths):
+            display_value = 'N/A' if value is None else value
             row_html += f"<div style='display:inline-block;width:{width}%;text-align:left;box-sizing: border-box;'>"
-            row_html += f"<p style='color:black;margin:0;font-size:14px;'>{value}</p>"
+            row_html += f"<p style='color:black;margin:0;font-size:14px;'>{display_value}</p>"
             row_html += "</div>"
 
         row_html += "</div>"
         st.markdown(row_html, unsafe_allow_html=True)
+
