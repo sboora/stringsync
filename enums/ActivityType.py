f9ebdd067b0c989579021cd43442a0b128eca700
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-11-22 13:26:44 -0800

    Added notifications. Changed User Experience

--- enums/ActivityType.py
+++ enums/ActivityType.py
@@ -2,9 +2,28 @@ import enum
 
 
 class ActivityType(enum.Enum):
-    LOG_IN = "Log In"
-    LOG_OUT = "Log Out"
-    PLAY_TRACK = "Play Track"
-    UPLOAD_RECORDING = "Upload Recording"
-    REGISTER_TUTOR = "Register Tutor"
-    REGISTER_SCHOOL = "Register School"
+    LOG_IN = ("Log In", "logged in", "🔓")
+    LOG_OUT = ("Log Out", "logged out", "🔒")
+    PLAY_TRACK = ("Play Track", "played a track", "▶️")
+    UPLOAD_RECORDING = ("Upload Recording", "uploaded a recording", "🎤")
+    REGISTER_TUTOR = ("Register Tutor", "registered a tutor", "👨‍🏫")
+    REGISTER_SCHOOL = ("Register School", "registered a school", "🏫")
+    POST_MESSAGE = ("Post Message", "posted a message", "✉️")
+
+    @classmethod
+    def from_value(cls, value):
+        for member in cls:
+            if member.value == value:
+                return member
+
+    @property
+    def value(self):
+        return self._value_[0]
+
+    @property
+    def message(self):
+        return self._value_[1]
+
+    @property
+    def icon(self):
+        return self._value_[2]
