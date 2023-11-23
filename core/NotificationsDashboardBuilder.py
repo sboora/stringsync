9e4e7a87960db74e7a544a948498214284168a6f
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-11-22 21:19:23 -0800

    Notifications

--- core/NotificationsDashboardBuilder.py
+++ core/NotificationsDashboardBuilder.py
@@ -1,3 +1,5 @@
+import base64
+import json
 import time
 
 import streamlit as st
@@ -10,15 +12,18 @@ class NotificationsDashboardBuilder:
         self.user_session_repo = user_session_repo
         self.portal_repo = portal_repo
 
-    def notify(self, user_id, group_id, session_id):
-        # Assume get_last_activity_time is a method that retrieves the last activity timestamp
-        last_activity_time = self.user_session_repo.get_last_activity_time(session_id)
-
+    def notify(self, user_id, group_id, last_activity_time):
         # Fetch new notifications since the last activity time
         notifications = self.portal_repo.get_notifications(user_id, group_id, last_activity_time)
 
         # Display each notification
+        messages = []
         for notification in notifications:
+            metadata = json.loads(notification.get('metadata', '{}'))
+            # If metadata contains a user_id, skip if it doesn't match the current user_id
+            if 'user_id' in metadata and metadata['user_id'] != user_id:
+                continue
+
             # Get the user-friendly message and icon for the activity type
             activity_type = ActivityType.from_value(notification['activity_type'])
             activity_message = activity_type.message
@@ -26,8 +31,9 @@ class NotificationsDashboardBuilder:
 
             # Construct the display message
             message = f"{activity_icon} {notification['user_name']} {activity_message}"
+            messages.append(message)
 
-            # Display the message with an icon
-            st.toast(message)
+        return messages
+
 
 
