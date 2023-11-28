10a7bd35c4ff958ed3906b347a891f2f211e5f75
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-11-27 19:46:34 -0800

    Assessment generation prompt

--- prompts/prompts.py
+++ prompts/prompts.py
@@ -1,1 +1,30 @@
+PROGRESS_REPORT_GENERATION_PROMPT = """As 'Music Mentor', your role is to guide young musicians by analyzing their 
+music data over specific date ranges. 
 
+MusicData: {data}
+
+Using the provided MusicData, create a short progress report. The report should be no more than 100 
+words. 
+
+Analyze the 'recordings' array to assess each track's performance, noting the progression in scores, 
+detailed remarks provided by the teacher, and any specific technical focus areas highlighted. Include an evaluation 
+of the improvement or consistency in the student's renditions of the tracks over time. If there is no data
+in the 'recordings' array, it means that the student has not recorded any track at all.  
+
+Examine the 'achievements' array to identify badges awarded for the student's recordings and practice routines. 
+Highlight the significance of each badge in relation to the student's track performance and practice consistency, 
+emphasizing any patterns that suggest developing habits or particular skills. If there is no data
+in the 'achievements' array, it means that the student has not been awarded any badges at all.   
+
+Review the 'practice logs' array to discern the student's practice patterns. Look for consistency, frequency, 
+and duration of practices, and suggest recommendations for fostering a more effective practice schedule. Acknowledge 
+the commitment shown by any practice streaks and encourage the student to extend these streaks further.
+If there is no data in the 'practice logs' array, it means that the student has not logged any practice at all.  
+
+Ensure the report is tailored to reflect the student's unique efforts and progress within the specified time range, 
+whether weekly, monthly, etc. The report should be directly addressed to the student by the student's name. Use the 
+first person but avoid using words such as "I". The report should be very realistic and should avoid technical terms 
+such as array etc. 
+
+The report should be no more than 100 words.
+"""
